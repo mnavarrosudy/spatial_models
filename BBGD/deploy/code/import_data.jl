79524d74052a0402422ae863ab5c2088ea129b06
Matias <51677324+mnavarrosudy@users.noreply.github.com>
2023-07-04 16:35:44 -0400

    update files for server

--- BBGD/deploy/code/import_data.jl
+++
@@ -1,9 +0,0 @@
-function import_data()
-
-    dist0 = Matrix(CSV.read("/Users/mnavarrosudy/Dropbox/bridges_bangladesh/dist0.csv", DataFrame, header=false))
-    dist1 = Matrix(CSV.read("/Users/mnavarrosudy/Dropbox/bridges_bangladesh/dist1.csv", DataFrame, header=false))
-    a = Matrix(CSV.read("/Users/mnavarrosudy/Dropbox/bridges_bangladesh/a.csv", DataFrame, header=false))
-    b = Matrix(CSV.read("/Users/mnavarrosudy/Dropbox/bridges_bangladesh/b.csv", DataFrame, header=false))
-
-    return dist0, dist1, a, b
-end
